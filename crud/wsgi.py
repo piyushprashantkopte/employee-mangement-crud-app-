4b1667bdc774f8f4cfe40f7c58842f11e73dbfbe
piyushprashantkopte <piyushkopte23@gmail.com>
2026-04-20 16:54:24 +0530

    'user-config'

--- crud/wsgi.py
+++ crud/wsgi.py
@@ -14,3 +14,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud.settings')
 
 application = get_wsgi_application()
+
+
+import os
+from django.contrib.auth import get_user_model
+
+# Get the User model
+User = get_user_model()
+
+# Check if the superuser already exists to avoid errors
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='piyush',
+        email='admin@example.com',
+        password='StrongPassword123'  
+    )
+    print("Superuser created successfully!")
